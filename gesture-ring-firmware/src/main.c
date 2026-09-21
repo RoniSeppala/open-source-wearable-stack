ce1ede1694c413a4154d9a864d8c81ef3c23d24c
Roni Seppälä <roni.o.seppala@gmail.com>
2026-09-21 23:24:06 +0300

    added simple test program for firmware

--- gesture-ring-firmware/src/main.c
+++ gesture-ring-firmware/src/main.c
@@ -4,13 +4,28 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
+#include <zephyr/drivers/sensor.h>
+#include <stdio.h>
 
 #define LED_NODE DT_ALIAS(led_strip)
+#define IMU_NODE DT_ALIAS(imu)
 
 static const struct device *const strip = DEVICE_DT_GET(LED_NODE);
+static const struct device *const imu = DEVICE_DT_GET(IMU_NODE);
 
 int main(void)
 {
+    struct sensor_value accel_x;
+    struct sensor_value accel_y;
+    struct sensor_value accel_z;
+
+    struct sensor_value gyro_x;
+    struct sensor_value gyro_y;
+    struct sensor_value gyro_z;
+
+    struct sensor_value die_temp;
+
+    int err;
     struct led_rgb pixel = {
         .r = 20,
         .g = 0,
@@ -18,13 +33,21 @@ int main(void)
     };
 
     if (!device_is_ready(strip)) {
+        printf("LED strip not ready\n");
+        return 0;
+    }
+
+    if (!device_is_ready(imu)) {
+        printf("IMU not ready\n");
         return 0;
     }
 
     while (1) {
+
+        // led blink
         led_strip_update_rgb(strip, &pixel, 1);
 
-        k_sleep(K_MSEC(500));
+        k_sleep(K_MSEC(50));
 
         pixel.r = 0;
         pixel.g = 0;
@@ -32,9 +55,66 @@ int main(void)
 
         led_strip_update_rgb(strip, &pixel, 1);
 
-        k_sleep(K_MSEC(500));
+        k_sleep(K_MSEC(50));
 
         pixel.r = 20;
+
+        // accelerometer checs
+
+        err = sensor_sample_fetch(imu);
+        if (err < 0 ) {
+            printf("Sample fetch error: %d\n", err);
+            return 0;
+        }
+
+        err = sensor_channel_get (imu, SENSOR_CHAN_ACCEL_X, &accel_x);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+        err = sensor_channel_get (imu, SENSOR_CHAN_ACCEL_Y, &accel_y);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+        err = sensor_channel_get (imu, SENSOR_CHAN_ACCEL_Z, &accel_z);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+
+        err = sensor_channel_get (imu, SENSOR_CHAN_GYRO_X, &gyro_x);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+        err = sensor_channel_get (imu, SENSOR_CHAN_GYRO_Y, &gyro_y);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+        err = sensor_channel_get (imu, SENSOR_CHAN_GYRO_Z, &gyro_z);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+
+        err = sensor_channel_get (imu, SENSOR_CHAN_DIE_TEMP, &die_temp);
+        if (err < 0) {
+            printf("Channel get error: %d\n", err);
+            return 0;
+        }
+
+        printf("\r\n\r\n\r\n");
+        printf("Accel x, %d.%06d\r\n", accel_x.val1, accel_x.val2);
+        printf("Accel y, %d.%06d\r\n", accel_y.val1, accel_y.val2);
+        printf("Accel z, %d.%06d\r\n", accel_z.val1, accel_z.val2);
+
+        printf("Gyro x, %d.%06d\r\n", gyro_x.val1, gyro_x.val2);
+        printf("Gyro y, %d.%06d\r\n", gyro_y.val1, gyro_y.val2);
+        printf("Gyro z, %d.%06d\r\n", gyro_z.val1, gyro_z.val2);
+
+        printf("Temp, %d.%06d\r\n", die_temp.val1, die_temp.val2);
     }
 
     return 0;
