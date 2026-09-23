51c25a99de70d16df7d244d2446b84135b794c22
Roni Seppälä <roni.o.seppala@gmail.com>
2026-09-23 17:51:36 +0300

    added acceleration normalisation to gesture test

--- gesture-ring-firmware/src/gesture_test.c
+++ gesture-ring-firmware/src/gesture_test.c
@@ -12,8 +12,11 @@
 
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/device.h>
+#include <math.h>
+#include <errno.h>
+#include <stdio.h>
 
-static int acceleration_normalisation(struct gyro_data *pre_normalisation_data, struct gyro_data *post_normalisation_data){
+static void acceleration_normalisation(struct all_gyro_data_int64_t *pre_normalisation_data, struct all_imu_data_accel_normalised_float_gyro_int64_t *post_normalisation_data){
     /*
     magnitude = sqrt(ax*ax + ay*ay + az*az);
 
@@ -22,12 +25,31 @@ static int acceleration_normalisation(struct gyro_data *pre_normalisation_data, 
     gz = az / magnitude;
     */
 
-    
+    float accel_x = (float)pre_normalisation_data->accel_x;
+    float accel_y = (float)pre_normalisation_data->accel_y;
+    float accel_z = (float)pre_normalisation_data->accel_z;
+
+    int64_t magnitude = sqrt(accel_x*accel_x+accel_y*accel_y+accel_z*accel_z);
+
+        if (magnitude == 0.0f) {
+        post_normalisation_data->accel_x = 0.0f;
+        post_normalisation_data->accel_y = 0.0f;
+        post_normalisation_data->accel_z = 0.0f;
+        return;
+    }
+
+    post_normalisation_data->accel_x = pre_normalisation_data->accel_x / magnitude;
+    post_normalisation_data->accel_y = pre_normalisation_data->accel_y / magnitude;
+    post_normalisation_data->accel_z = pre_normalisation_data->accel_z / magnitude;
+
+    post_normalisation_data->gyro_x = pre_normalisation_data->gyro_x;
+    post_normalisation_data->gyro_y = pre_normalisation_data->gyro_y;
+    post_normalisation_data->gyro_z = pre_normalisation_data->gyro_z;
+
+    post_normalisation_data->die_temp = pre_normalisation_data->die_temp;
 }
 
-static int gyro_data_sensor_value_to_int64_t(struct gyro_data *pre_transform, struct all_gyro_data_int64_t *post_transform){
-    int err;
-
+static void gyro_data_sensor_value_to_int64_t(const struct gyro_data *pre_transform, struct all_gyro_data_int64_t *post_transform){
     post_transform->accel_x = sensor_value_to_micro(&pre_transform->accel_x);
     post_transform->accel_y = sensor_value_to_micro(&pre_transform->accel_y);
     post_transform->accel_z = sensor_value_to_micro(&pre_transform->accel_z);
@@ -37,8 +59,6 @@ static int gyro_data_sensor_value_to_int64_t(struct gyro_data *pre_transform, st
     post_transform->gyro_z = sensor_value_to_micro(&pre_transform->gyro_z);
 
     post_transform->die_temp = sensor_value_to_micro(&pre_transform->die_temp);
-
-    return 0;
 }
 
 int get_gesture(const struct device *imu){
@@ -46,7 +66,7 @@ int get_gesture(const struct device *imu){
     struct gyro_data pre_calibration_data_sensor_value;
 
     struct all_gyro_data_int64_t pre_calibration_data;
-    struct all_gyro_data_int64_t post_calibration_data;
+    struct all_imu_data_accel_normalised_float_gyro_int64_t post_calibration_data;
 
     err = get_all_gyro_data(imu, &pre_calibration_data_sensor_value);
     if (err < 0) {
@@ -54,7 +74,9 @@ int get_gesture(const struct device *imu){
         return -ENODATA;
     }
 
-    err = gyro_data_sensor_value_to_int64_t(&pre_calibration_data_sensor_value, &pre_calibration_data);
+    gyro_data_sensor_value_to_int64_t(&pre_calibration_data_sensor_value, &pre_calibration_data);
+
+    acceleration_normalisation(&pre_calibration_data, &post_calibration_data);
 
     return 0;
 }
--- gesture-ring-firmware/src/gesture_test.h
+++ gesture-ring-firmware/src/gesture_test.h
@@ -20,4 +20,16 @@ struct all_gyro_data_int64_t {
     int64_t die_temp;
 };
 
+struct all_imu_data_accel_normalised_float_gyro_int64_t {
+    float accel_x;
+    float accel_y;
+    float accel_z;
+
+    int64_t gyro_x;
+    int64_t gyro_y;
+    int64_t gyro_z;
+
+    int64_t die_temp;
+};
+
 #endif
